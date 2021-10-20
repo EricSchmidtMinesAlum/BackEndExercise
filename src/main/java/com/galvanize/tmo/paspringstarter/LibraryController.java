9075f5dd8e1155f76370ace4f16c22cc960f00e3
Schmidt <Eric.Schmidt3@T-Mobile.com>
2021-10-20 16:29:00 -0600

    added responce code

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -3,12 +3,15 @@ package com.galvanize.tmo.paspringstarter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,6 +52,7 @@ public class LibraryController {
     */
     
     @PostMapping(value="/api/books")
+    @ResponseStatus(code = HttpStatus.CREATED, reason = "CREATE")
         Books newBooks(@RequestBody Books newBooks) {
         return repository.save(newBooks);
     }
