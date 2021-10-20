0e1eb672592e31a074aa945099444eccd7db6de5
Schmidt <Eric.Schmidt3@T-Mobile.com>
2021-10-20 08:40:35 -0600

    Added a practice location

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -2,12 +2,18 @@ package com.galvanize.tmo.paspringstarter;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class LibraryController {
 
     @GetMapping("/health")
     public void health() {
-        return 101;
+    
     }
+    
+    @RequestMapping("/api/books")
+	public @ResponseBody String greeting() {
+		return "Hello, World";
+	}
 }
