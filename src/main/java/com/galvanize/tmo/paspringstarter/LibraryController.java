10c103f4a8d0dc50a47649350d4e28387755b326
Schmidt <Eric.Schmidt3@T-Mobile.com>
2021-10-20 15:41:40 -0600

    trying requestMethod

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,12 +40,19 @@ public class LibraryController {
         List<Books> all() {
         return repository.findAll();
         }
-
+    
+    @RequestMapping(value="/api/books", method = RequestMethod.POST)
+    Books newBooks(@RequestBody Books newBooks) {
+        repository.save(newBooks);
+        return newBooks;
+    }
+    
+    /*
     @PostMapping(value="/api/books",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
         Books newBooks(@RequestBody Books newBooks) {
         repository.save(newBooks);
         return newBooks;
-    }
+    }*/
     
 } 
 
