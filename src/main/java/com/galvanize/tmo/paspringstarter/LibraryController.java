6caff6d2a113d6747320ee213a0f8e3a9dbc81b6
Schmidt <Eric.Schmidt3@T-Mobile.com>
2021-10-20 14:59:51 -0600

    Added content for post

--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +40,7 @@ public class LibraryController {
         return repository.findAll();
         }
 
-    @PostMapping("/api/books")
+    @PostMapping(value="/api/books",consumes = MediaType.APPLICATION_JSON_VALUE)
         Books newBooks(@RequestBody Books newBooks) {
         return repository.save(newBooks);
     }
